8f97b78ebd5f7a4a48b0eb338ecc3756bb7d9169
Tiffceet <zkang.loo@gmail.com>
2022-06-19 12:54:25 +0800

    Add: submitScoreIndividual endpoint

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import * as cors from "cors";
 import express from "express";
 import path from "path";
 import bodyParser from "body-parser";
+import { resourceLimits } from "worker_threads";
+import { resolveSoa } from "dns";
 
 const DIFF_MAP = {
     Novice: "Beginner",
@@ -35,7 +37,8 @@ const TIER_MAP = {
 };
 
 const app = express();
-app.use(bodyParser.text({ type: "*/*" }));
+app.use(bodyParser.text({ type: "text/plain" }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors.default());
 
 // curl -H "Content-Type: text/plain; charset=UTF-8" --data-binary "@Stats.xml" konishi.tk:8765/submitScore
@@ -60,7 +63,213 @@ app.post("/submitScore", function (req, res) {
             JSON.stringify(data)
         );
     });
-    res.send("Done !");
+    res.status(200);
+    res.end();
+});
+
+// curl -X POST --data "player=Looz (Controller)&pack=DDR 2014 (AC) (Japan)&song=EGOISM 440&Difficulty=Hard&DateTime=2022-06-18 21:10:53&Disqualified=false&Grade=Tier07&MaxCombo=88&Modifiers=Overhead, Ddrx-note&PercentDP=0.577&Score=999999&HitMine=0&AvoidMine=0&CheckpointMiss=0&Miss=0&W5=0&W4=0&W3=0&W2=0&W1=0&CheckpointHit=0&OK=0" localhost:8765/submitScoreIndividual
+/**
+ *
+ */
+app.post("/submitScoreIndividual", function (req, res) {
+    let {
+        player,
+        pack,
+        song,
+        Difficulty,
+        DateTime,
+        Disqualified,
+        Grade,
+        MaxCombo,
+        Modifiers,
+        PercentDP,
+        Score,
+        HitMine,
+        AvoidMine,
+        CheckpointMiss,
+        Miss,
+        W5,
+        W4,
+        W3,
+        W2,
+        W1,
+        CheckpointHit,
+        OK,
+    } = req.body;
+    Disqualified = Disqualified === "true" ? true : false;
+    Modifiers = Modifiers.split(",");
+    Score = +Score;
+    HitMine = +HitMine;
+    AvoidMine = +AvoidMine;
+    CheckpointMiss = +CheckpointMiss;
+    Miss = +Miss;
+    W5 = +W5;
+    W4 = +W4;
+    W3 = +W3;
+    W2 = +W2;
+    W1 = +W1;
+    CheckpointHit = +CheckpointHit;
+    OK = +OK;
+
+    if (!verifyIndividualScoreBody(req.body)) {
+        res.status(400);
+        res.send("400 Bad Request");
+        return;
+    }
+
+    let score_obj = {};
+    try {
+        score_obj = JSON.parse(
+            fs.readFileSync(
+                `${path.resolve(path.dirname(""))}${path.sep}scores${
+                    path.sep
+                }${player}.json`
+            )
+        );
+    } catch (e) {
+        // This is a new player
+        score_obj = {};
+        score_obj[player] = [];
+    }
+
+    let player_key = Object.keys(score_obj)[0];
+    let scores_arr = score_obj[player_key];
+    let found_song = [false, -1];
+    for (let i = 0; i < scores_arr.length; i++) {
+        const x = scores_arr[i];
+        if (x.pack === pack && x.song === song) {
+            found_song[0] = true;
+            found_song[1] = i;
+            break;
+        }
+    }
+
+    if (!found_song[0]) {
+        score_obj[player_key].push({
+            pack,
+            song,
+            diffs: [
+                {
+                    Difficulty,
+                    DateTime,
+                    Disqualified,
+                    Grade,
+                    MaxCombo,
+                    Modifiers,
+                    PercentDP,
+                    Score,
+                    TapNoteScores: {
+                        HitMine,
+                        AvoidMine,
+                        CheckpointMiss,
+                        Miss,
+                        W5,
+                        W4,
+                        W3,
+                        W2,
+                        W1,
+                        CheckpointHit,
+                        OK,
+                    },
+                },
+            ],
+        });
+        fs.writeFileSync(
+            `${path.resolve(path.dirname(""))}${path.sep}scores${
+                path.sep
+            }${player}.json`,
+            JSON.stringify(score_obj)
+        );
+        res.status(200);
+        res.end();
+        return;
+    }
+
+    let diffs = score_obj[player_key][found_song[1]].diffs;
+    let found_diff = [false, -1];
+    for (let i = 0; i < diffs.length; i++) {
+        const x = diffs[i];
+        if (x.Difficulty === Difficulty) {
+            found_diff = [true, i];
+            break;
+        }
+    }
+
+    if (!found_diff[0]) {
+        score_obj[player_key][found_song[1]].diffs.push({
+            Difficulty,
+            DateTime,
+            Disqualified,
+            Grade,
+            MaxCombo,
+            Modifiers,
+            PercentDP,
+            Score,
+            TapNoteScores: {
+                HitMine,
+                AvoidMine,
+                CheckpointMiss,
+                Miss,
+                W5,
+                W4,
+                W3,
+                W2,
+                W1,
+                CheckpointHit,
+                OK,
+            },
+        });
+        fs.writeFileSync(
+            `${path.resolve(path.dirname(""))}${path.sep}scores${
+                path.sep
+            }${player}.json`,
+            JSON.stringify(score_obj)
+        );
+        res.status(200);
+        res.end();
+        return;
+    }
+
+    let existing_score =
+        score_obj[player_key][found_song[1]].diffs[found_diff[1]].Score;
+    if (existing_score > Score) {
+        res.status(200);
+        res.end();
+        return;
+    }
+
+    score_obj[player_key][found_song[1]].diffs[found_diff[1]] = {
+        Difficulty,
+        DateTime,
+        Disqualified,
+        Grade,
+        MaxCombo,
+        Modifiers,
+        PercentDP,
+        Score,
+        TapNoteScores: {
+            HitMine,
+            AvoidMine,
+            CheckpointMiss,
+            Miss,
+            W5,
+            W4,
+            W3,
+            W2,
+            W1,
+            CheckpointHit,
+            OK,
+        },
+    };
+
+    fs.writeFileSync(
+        `${path.resolve(path.dirname(""))}${path.sep}scores${
+            path.sep
+        }${player}.json`,
+        JSON.stringify(score_obj)
+    );
+    res.status(200);
+    res.end();
 });
 
 app.get("/", function (req, res) {
@@ -91,6 +300,138 @@ app.get("/getScores", async function (req, res) {
         res.json(convertScoreIntoDataRow(scores));
     });
 });
+
+const verifyIndividualScoreBody = (body) => {
+    console.log("Debug: ");
+    console.log(JSON.stringify(body));
+    let {
+        player,
+        pack,
+        song,
+        Difficulty,
+        DateTime,
+        Disqualified,
+        Grade,
+        MaxCombo,
+        Modifiers,
+        PercentDP,
+        Score,
+        HitMine,
+        AvoidMine,
+        CheckpointMiss,
+        Miss,
+        W5,
+        W4,
+        W3,
+        W2,
+        W1,
+        CheckpointHit,
+        OK,
+    } = body;
+    if (typeof player === "undefined") {
+        return false;
+    }
+    if (typeof pack === "undefined") {
+        return false;
+    }
+    if (typeof song === "undefined") {
+        return false;
+    }
+    if (typeof Difficulty === "undefined") {
+        return false;
+    }
+    if (typeof DateTime === "undefined") {
+        return false;
+    }
+    if (typeof Disqualified === "undefined") {
+        return false;
+    }
+    if (typeof Grade === "undefined") {
+        return false;
+    }
+    if (typeof MaxCombo === "undefined") {
+        return false;
+    }
+    if (typeof Modifiers === "undefined") {
+        return false;
+    }
+    if (typeof PercentDP === "undefined") {
+        return false;
+    }
+    if (typeof Score === "undefined") {
+        return false;
+    }
+    if (typeof HitMine === "undefined") {
+        return false;
+    }
+    if (typeof AvoidMine === "undefined") {
+        return false;
+    }
+    if (typeof CheckpointMiss === "undefined") {
+        return false;
+    }
+    if (typeof Miss === "undefined") {
+        return false;
+    }
+    if (typeof W5 === "undefined") {
+        return false;
+    }
+    if (typeof W4 === "undefined") {
+        return false;
+    }
+    if (typeof W3 === "undefined") {
+        return false;
+    }
+    if (typeof W2 === "undefined") {
+        return false;
+    }
+    if (typeof W1 === "undefined") {
+        return false;
+    }
+    if (typeof CheckpointHit === "undefined") {
+        return false;
+    }
+    if (typeof OK === "undefined") {
+        return false;
+    }
+    if (isNaN(Score)) {
+        return false;
+    }
+    if (isNaN(HitMine)) {
+        return false;
+    }
+    if (isNaN(AvoidMine)) {
+        return false;
+    }
+    if (isNaN(CheckpointMiss)) {
+        return false;
+    }
+    if (isNaN(Miss)) {
+        return false;
+    }
+    if (isNaN(W5)) {
+        return false;
+    }
+    if (isNaN(W4)) {
+        return false;
+    }
+    if (isNaN(W3)) {
+        return false;
+    }
+    if (isNaN(W2)) {
+        return false;
+    }
+    if (isNaN(W1)) {
+        return false;
+    }
+    if (isNaN(CheckpointHit)) {
+        return false;
+    }
+    if (isNaN(OK)) {
+        return false;
+    }
+    return true;
+};
 
 const convertScoreIntoDataRow = (scores) => {
     let datarows = [];
