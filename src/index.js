a21ed4e60c524e96855ccf308ecc7ebdbb3d59fd
Tiffceet <zkang.loo@gmail.com>
2022-06-20 11:46:44 +0800

    Make getScores queryable

--- src/index.js
+++ src/index.js
@@ -310,11 +310,14 @@ app.get("/", function (req, res) {
 
 // curl localhost:3000/getScores?username=Controller
 app.get("/getScores", async function (req, res) {
-    if (req.query.username) {
-        // Do filtering
-    }
+    let { r_song, r_pack, r_diff } = req.query;
     let dirname = path.resolve(path.dirname("")) + path.sep;
     let scores = {};
+
+    let aliases = {};
+    try {
+        aliases = JSON.parse(fs.readFileSync(dirname + "alias.json"));
+    } catch (e) {}
 
     fs.readdir(`${dirname}${path.sep}scores`, function (err, filenames) {
         if (err) {
@@ -329,7 +332,7 @@ app.get("/getScores", async function (req, res) {
             );
             scores = { ...scores, ...JSON.parse(content) };
         }
-        res.json(convertScoreIntoDataRow(scores));
+        res.json(convertScoreIntoDataRow(scores, aliases, req.query));
     });
 });
 
@@ -465,25 +468,47 @@ const verifyIndividualScoreBody = (body) => {
     return true;
 };
 
-const convertScoreIntoDataRow = (scores) => {
+const convertScoreIntoDataRow = (
+    scores,
+    packAlias,
+    { r_song, r_pack, r_diff }
+) => {
     let datarows = [];
     scores = Object.entries(scores);
     scores.forEach(([player, songs]) => {
         songs.forEach((song) => {
             song.diffs.forEach((diff) => {
+                // Remap pack, diff and grade
+                let cur_pack = song.pack;
+                if (!packAlias[player] && !packAlias[player][song.pack]) {
+                    cur_pack = packAlias[player][song.pack];
+                }
+                let cur_diff =
+                    typeof DIFF_MAP[diff.Difficulty] === "undefined"
+                        ? diff.Difficulty
+                        : DIFF_MAP[diff.Difficulty];
+                let cur_grade =
+                    typeof TIER_MAP[diff.Grade] === "undefined"
+                        ? diff.Grade
+                        : TIER_MAP[diff.Grade];
+                if (r_song && song.song !== r_song) {
+                    return;
+                }
+                if (r_pack) {
+                    if (cur_pack !== r_pack) {
+                        return;
+                    }
+                }
+                if (r_diff && r_diff !== cur_diff) {
+                    return;
+                }
                 datarows.push({
                     DateTime: diff.DateTime,
                     PlayerName: player,
                     SongPack: song.pack,
                     SongName: song.song,
-                    Difficulty:
-                        typeof DIFF_MAP[diff.Difficulty] === "undefined"
-                            ? diff.Difficulty
-                            : DIFF_MAP[diff.Difficulty],
-                    Grade:
-                        typeof TIER_MAP[diff.Grade] === "undefined"
-                            ? diff.Grade
-                            : TIER_MAP[diff.Grade],
+                    Difficulty: cur_diff,
+                    Grade: cur_grade,
                     Score: diff.Score,
                     PercentDP: diff.PercentDP,
                     MaxCombo: diff.MaxCombo,
