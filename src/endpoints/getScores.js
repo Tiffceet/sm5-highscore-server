b30f180ea54c2ebc4fa14c5c8089e46748b7dfce
Tiffceet <zkang.loo@gmail.com>
2024-10-27 21:07:10 +0800

    chore: prettier

--- src/endpoints/getScores.js
+++ src/endpoints/getScores.js
@@ -2,149 +2,145 @@ import { dirname, DIFF_MAP, TIER_MAP, FC_MAP } from "../globals.js";
 import * as fs from "fs";
 import path from "path";
 const getScores = {
-    method: "get",
-    endpoint: "/getScores",
-    handler: async function (req, res) {
-        let { r_song, r_pack, r_diff, sort, score_type } = req.query;
-        let scores = {};
+  method: "get",
+  endpoint: "/getScores",
+  handler: async function (req, res) {
+    let { r_song, r_pack, r_diff, sort, score_type } = req.query;
+    let scores = {};
 
-        let aliases = {};
-        try {
-            aliases = JSON.parse(fs.readFileSync(dirname + "alias.json"));
-        } catch (e) {}
+    let aliases = {};
+    try {
+      aliases = JSON.parse(fs.readFileSync(dirname + "alias.json"));
+    } catch (e) {}
 
-        fs.readdir(`${dirname}scores`, function (err, filenames) {
-            if (err) {
-                console.error(err);
-                return;
-            }
-            for (let i = 0; i < filenames.length; i++) {
-                const filename = filenames[i];
-                let content = fs.readFileSync(
-                    dirname + "scores" + path.sep + filename,
-                    "utf-8"
-                );
-                content = JSON.parse(content);
-                if (score_type) {
-                    if (content.score_type != score_type) {
-                        continue;
-                    }
-                }
-                let new_score_obj = {};
-                new_score_obj[`${content.username}`] = content.scores;
-                let content_score_type = content.score_type;
-                if (!scores[content_score_type]) {
-                    scores[content_score_type] = {};
-                }
-                scores[content_score_type] = {
-                    ...scores[content_score_type],
-                    ...new_score_obj,
-                };
-            }
-            let data_arr = convertScoreIntoDataRow(scores, aliases, req.query);
-            if (sort === "desc") {
-                data_arr.sort((a, b) => {
-                    if (a.Score < b.Score) {
-                        return 1;
-                    }
-                    if (a.Score > b.Score) {
-                        return -1;
-                    }
-                    if (a.Score == b.Score) {
-                        return 0;
-                    }
-                });
-            }
-            if (sort === "asc") {
-                data_arr.sort((a, b) => {
-                    if (a.Score > b.Score) {
-                        return 1;
-                    }
-                    if (a.Score < b.Score) {
-                        return -1;
-                    }
-                    if (a.Score == b.Score) {
-                        return 0;
-                    }
-                });
-            }
-            res.json(data_arr);
+    fs.readdir(`${dirname}scores`, function (err, filenames) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      for (let i = 0; i < filenames.length; i++) {
+        const filename = filenames[i];
+        let content = fs.readFileSync(
+          dirname + "scores" + path.sep + filename,
+          "utf-8"
+        );
+        content = JSON.parse(content);
+        if (score_type) {
+          if (content.score_type != score_type) {
+            continue;
+          }
+        }
+        let new_score_obj = {};
+        new_score_obj[`${content.username}`] = content.scores;
+        let content_score_type = content.score_type;
+        if (!scores[content_score_type]) {
+          scores[content_score_type] = {};
+        }
+        scores[content_score_type] = {
+          ...scores[content_score_type],
+          ...new_score_obj,
+        };
+      }
+      let data_arr = convertScoreIntoDataRow(scores, aliases, req.query);
+      if (sort === "desc") {
+        data_arr.sort((a, b) => {
+          if (a.Score < b.Score) {
+            return 1;
+          }
+          if (a.Score > b.Score) {
+            return -1;
+          }
+          if (a.Score == b.Score) {
+            return 0;
+          }
         });
-    },
+      }
+      if (sort === "asc") {
+        data_arr.sort((a, b) => {
+          if (a.Score > b.Score) {
+            return 1;
+          }
+          if (a.Score < b.Score) {
+            return -1;
+          }
+          if (a.Score == b.Score) {
+            return 0;
+          }
+        });
+      }
+      res.json(data_arr);
+    });
+  },
 };
 
 const convertScoreIntoDataRow = (
-    scores,
-    packAlias,
-    { r_song, r_pack, r_diff }
+  scores,
+  packAlias,
+  { r_song, r_pack, r_diff }
 ) => {
-    let datarows = [];
+  let datarows = [];
 
-    let scores_by_type = Object.entries(scores);
-    scores_by_type.forEach(([score_type, scores]) => {
-        scores = Object.entries(scores);
-        scores.forEach(([player, songs]) => {
-            songs.forEach((song) => {
-                song.diffs.forEach((diff) => {
-                    // Remap pack, diff and grade
-                    let cur_pack = song.pack;
-                    if (packAlias[player] && packAlias[player][song.pack]) {
-                        cur_pack = packAlias[player][song.pack];
-                    }
-                    let cur_diff =
-                        typeof DIFF_MAP[diff.Difficulty] === "undefined"
-                            ? diff.Difficulty
-                            : DIFF_MAP[diff.Difficulty];
-                    let cur_grade =
-                        typeof TIER_MAP[diff.Grade] === "undefined"
-                            ? diff.Grade
-                            : TIER_MAP[diff.Grade];
-                    if (r_song && song.song !== r_song) {
-                        return;
-                    }
-                    if (r_pack) {
-                        if (cur_pack !== r_pack) {
-                            return;
-                        }
-                    }
-                    if (r_diff && r_diff !== cur_diff) {
-                        return;
-                    }
+  let scores_by_type = Object.entries(scores);
+  scores_by_type.forEach(([score_type, scores]) => {
+    scores = Object.entries(scores);
+    scores.forEach(([player, songs]) => {
+      songs.forEach((song) => {
+        song.diffs.forEach((diff) => {
+          // Remap pack, diff and grade
+          let cur_pack = song.pack;
+          if (packAlias[player] && packAlias[player][song.pack]) {
+            cur_pack = packAlias[player][song.pack];
+          }
+          let cur_diff =
+            typeof DIFF_MAP[diff.Difficulty] === "undefined"
+              ? diff.Difficulty
+              : DIFF_MAP[diff.Difficulty];
+          let cur_grade =
+            typeof TIER_MAP[diff.Grade] === "undefined"
+              ? diff.Grade
+              : TIER_MAP[diff.Grade];
+          if (r_song && song.song !== r_song) {
+            return;
+          }
+          if (r_pack) {
+            if (cur_pack !== r_pack) {
+              return;
+            }
+          }
+          if (r_diff && r_diff !== cur_diff) {
+            return;
+          }
 
-                    if (
-                        diff.FCType &&
-                        diff.FCType !== "NONE" &&
-                        FC_MAP[diff.FCType]
-                    ) {
-                        cur_grade = cur_grade + FC_MAP[diff.FCType];
-                    }
+          if (diff.FCType && diff.FCType !== "NONE" && FC_MAP[diff.FCType]) {
+            cur_grade = cur_grade + FC_MAP[diff.FCType];
+          }
 
-                    datarows.push({
-                        DateTime: diff.DateTime,
-                        PlayerName: player,
-                        SongPack: song.pack,
-                        SongName: song.song,
-                        Difficulty: cur_diff,
-                        Grade: cur_grade,
-                        Score: diff.Score,
-                        PercentDP: diff.PercentDP,
-                        MaxCombo: diff.MaxCombo,
-                        Marvelous: diff.TapNoteScores.W1,
-                        Perfect: diff.TapNoteScores.W2,
-                        Great: diff.TapNoteScores.W3,
-                        Good: diff.TapNoteScores.W4,
-                        OK: diff.OK,
-                        Miss: diff.TapNoteScores.Miss,
-                        NG: typeof diff.NG === "number" ? diff.NG : "-",
-                        NumTimesPlayed: diff.NumTimesPlayed,
-                        Type: score_type,
-                        Modifiers: diff.Modifiers.join(", "),
-                    });
-                });
-            });
+          datarows.push({
+            DateTime: diff.DateTime,
+            PlayerName: player,
+            SongPack: song.pack,
+            SongName: song.song,
+            Difficulty: cur_diff,
+            Grade: cur_grade,
+            Score: diff.Score,
+            PercentDP: diff.PercentDP,
+            MaxCombo: diff.MaxCombo,
+            Marvelous: diff.TapNoteScores.W1,
+            Perfect: diff.TapNoteScores.W2,
+            Great: diff.TapNoteScores.W3,
+            Good: diff.TapNoteScores.W4,
+            OK: diff.OK,
+            Miss: diff.TapNoteScores.Miss,
+            NG: typeof diff.NG === "number" ? diff.NG : "-",
+            NumTimesPlayed: diff.NumTimesPlayed,
+            Type: score_type,
+            Modifiers: diff.Modifiers.join(", "),
+          });
         });
+      });
     });
-    return datarows;
+  });
+  return datarows;
 };
 
 export { getScores };
--- src/endpoints/submitScore.js
+++ src/endpoints/submitScore.js
@@ -4,106 +4,103 @@ import path from "path";
 import { dirname } from "../globals.js";
 // curl -H "Content-Type: text/plain; charset=UTF-8" --data-binary "@Stats.xml" localhost:8765/submitScore
 const submitScore = {
-    method: "post",
-    endpoint: "/submitScore",
-    handler: function (req, res) {
-        let score_type = "-";
-        if (req.query.score_type) {
-            score_type = req.query.score_type;
-        }
+  method: "post",
+  endpoint: "/submitScore",
+  handler: function (req, res) {
+    let score_type = "-";
+    if (req.query.score_type) {
+      score_type = req.query.score_type;
+    }
 
-        xml2js.parseString(req.body, (err, result) => {
-            if (err) {
-                console.error(err);
-                return;
-            }
+    xml2js.parseString(req.body, (err, result) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
 
-            let username = result.Stats.GeneralData[0].DisplayName[0];
-            let data = {
-                username,
-                score_type,
-                scores: [],
-            };
-            data[username] = [];
-            result.Stats.SongScores[0].Song.forEach((song_obj) => {
-                data.scores.push(parseSong(song_obj));
-            });
-            fs.writeFileSync(
-                `${dirname}scores${path.sep}${username}${
-                    score_type === "-" ? "" : "-" + score_type
-                }.json`,
-                JSON.stringify(data)
-            );
+      let username = result.Stats.GeneralData[0].DisplayName[0];
+      let data = {
+        username,
+        score_type,
+        scores: [],
+      };
+      data[username] = [];
+      result.Stats.SongScores[0].Song.forEach((song_obj) => {
+        data.scores.push(parseSong(song_obj));
+      });
+      fs.writeFileSync(
+        `${dirname}scores${path.sep}${username}${
+          score_type === "-" ? "" : "-" + score_type
+        }.json`,
+        JSON.stringify(data)
+      );
 
-            // Code to save a backup copy
-            fs.writeFileSync(
-                `${dirname}_backup${path.sep}${username}.xml`,
-                req.body
-            );
-        });
-        res.status(200);
-        res.end();
-    },
+      // Code to save a backup copy
+      fs.writeFileSync(`${dirname}_backup${path.sep}${username}.xml`, req.body);
+    });
+    res.status(200);
+    res.end();
+  },
 };
 
 const parseSong = (song_obj) => {
-    let diffs = [];
-    let [_, pack, song] = song_obj.$.Dir.split("/");
-    song_obj.Steps.forEach((diff_obj) => {
-        if (
-            typeof diff_obj.HighScoreList !== "undefined" &&
-            typeof diff_obj.HighScoreList[0].HighScore !== "undefined"
-        ) {
-            diffs.push(parseDiff(diff_obj));
-        }
-    });
-    return {
-        pack,
-        song,
-        diffs,
-    };
+  let diffs = [];
+  let [_, pack, song] = song_obj.$.Dir.split("/");
+  song_obj.Steps.forEach((diff_obj) => {
+    if (
+      typeof diff_obj.HighScoreList !== "undefined" &&
+      typeof diff_obj.HighScoreList[0].HighScore !== "undefined"
+    ) {
+      diffs.push(parseDiff(diff_obj));
+    }
+  });
+  return {
+    pack,
+    song,
+    diffs,
+  };
 };
 
 const parseDiff = (diff_obj) => {
-    let { Difficulty } = diff_obj.$;
-    let NumTimesPlayed = diff_obj.HighScoreList[0].NumTimesPlayed[0];
-    let top_score = diff_obj.HighScoreList[0].HighScore[0];
+  let { Difficulty } = diff_obj.$;
+  let NumTimesPlayed = diff_obj.HighScoreList[0].NumTimesPlayed[0];
+  let top_score = diff_obj.HighScoreList[0].HighScore[0];
 
-    let DateTime = top_score.DateTime[0];
-    let NG = +top_score.HoldNoteScores[0].LetGo[0];
-    // let Grade = top_score.Grade[0];
-    let Grade = getDDRAGradeFromScore(+top_score.Score[0], top_score.Grade[0]);
-    let MaxCombo = top_score.MaxCombo[0];
-    let FCType = getFCType(top_score);
-    let Modifiers = top_score.Modifiers;
-    let PercentDP = top_score.PercentDP[0];
-    let Score = +top_score.Score[0];
-    let TapNoteScores = Object.entries(top_score.TapNoteScores[0]).reduce(
-        (prev, cur, idx) => {
-            let new_obj = {
-                ...prev,
-            };
-            new_obj[cur[0]] = +cur[1][0];
-            return new_obj;
-        },
-        {}
-    );
-    TapNoteScores.W4 = TapNoteScores.W4 + TapNoteScores.W5;
-    let OK = +top_score.HoldNoteScores[0].Held[0];
-    return {
-        Difficulty,
-        DateTime,
-        NG,
-        Grade,
-        MaxCombo,
-        Modifiers,
-        PercentDP,
-        Score,
-        TapNoteScores,
-        OK,
-        FCType,
-        NumTimesPlayed
-    };
+  let DateTime = top_score.DateTime[0];
+  let NG = +top_score.HoldNoteScores[0].LetGo[0];
+  // let Grade = top_score.Grade[0];
+  let Grade = getDDRAGradeFromScore(+top_score.Score[0], top_score.Grade[0]);
+  let MaxCombo = top_score.MaxCombo[0];
+  let FCType = getFCType(top_score);
+  let Modifiers = top_score.Modifiers;
+  let PercentDP = top_score.PercentDP[0];
+  let Score = +top_score.Score[0];
+  let TapNoteScores = Object.entries(top_score.TapNoteScores[0]).reduce(
+    (prev, cur, idx) => {
+      let new_obj = {
+        ...prev,
+      };
+      new_obj[cur[0]] = +cur[1][0];
+      return new_obj;
+    },
+    {}
+  );
+  TapNoteScores.W4 = TapNoteScores.W4 + TapNoteScores.W5;
+  let OK = +top_score.HoldNoteScores[0].Held[0];
+  return {
+    Difficulty,
+    DateTime,
+    NG,
+    Grade,
+    MaxCombo,
+    Modifiers,
+    PercentDP,
+    Score,
+    TapNoteScores,
+    OK,
+    FCType,
+    NumTimesPlayed,
+  };
 };
 
 /**
@@ -112,82 +109,82 @@ const parseDiff = (diff_obj) => {
  * @returns {"NONE"|"MFC"|"PFC"|"GRFC"|"GOFC"}
  */
 const getFCType = (score) => {
-    // Attempt to read stage award before parsing the fc manually
-    if (score.StageAward && score.StageAward[0]) {
-        switch (score.StageAward[0]) {
-            case "FullComboW3":
-                return "GRFC";
-            case "FullComboW2":
-                return "PFC";
-            case "FullComboW1":
-                return "MFC";
-        }
+  // Attempt to read stage award before parsing the fc manually
+  if (score.StageAward && score.StageAward[0]) {
+    switch (score.StageAward[0]) {
+      case "FullComboW3":
+        return "GRFC";
+      case "FullComboW2":
+        return "PFC";
+      case "FullComboW1":
+        return "MFC";
     }
-    let max_combo = +score.RadarValues[0].TapsAndHolds[0];
-    let player_max_combo = score.MaxCombo[0];
-    if (player_max_combo < max_combo) {
-        return "NONE";
-    }
+  }
+  let max_combo = +score.RadarValues[0].TapsAndHolds[0];
+  let player_max_combo = score.MaxCombo[0];
+  if (player_max_combo < max_combo) {
+    return "NONE";
+  }
 
-    let fc_type = "MFC";
-    if (+score.TapNoteScores[0].W2[0] != 0) {
-        fc_type = "PFC";
-    }
+  let fc_type = "MFC";
+  if (+score.TapNoteScores[0].W2[0] != 0) {
+    fc_type = "PFC";
+  }
 
-    if (+score.TapNoteScores[0].W3[0] != 0) {
-        fc_type = "GRFC";
-    }
-    if (+score.TapNoteScores[0].W4[0] != 0) {
-        fc_type = "GOFC";
-    }
-    if (+score.TapNoteScores[0].W5[0] != 0) {
-        fc_type = "NONE";
-    }
-    if (+score.HoldNoteScores[0].LetGo[0] != 0) {
-        fc_type = "NONE";
-    }
-    if (+score.HoldNoteScores[0].MissedHold[0] != 0) {
-        fc_type = "NONE";
-    }
-    if (+score.TapNoteScores[0].Miss[0] != 0) {
-        fc_type = "NONE";
-    }
+  if (+score.TapNoteScores[0].W3[0] != 0) {
+    fc_type = "GRFC";
+  }
+  if (+score.TapNoteScores[0].W4[0] != 0) {
+    fc_type = "GOFC";
+  }
+  if (+score.TapNoteScores[0].W5[0] != 0) {
+    fc_type = "NONE";
+  }
+  if (+score.HoldNoteScores[0].LetGo[0] != 0) {
+    fc_type = "NONE";
+  }
+  if (+score.HoldNoteScores[0].MissedHold[0] != 0) {
+    fc_type = "NONE";
+  }
+  if (+score.TapNoteScores[0].Miss[0] != 0) {
+    fc_type = "NONE";
+  }
 
-    return fc_type;
+  return fc_type;
 };
 
 const getDDRAGradeFromScore = (score, original_grade) => {
-    if (original_grade === "Failed") {
-        return "Failed";
+  if (original_grade === "Failed") {
+    return "Failed";
+  }
+  let grade_table = {
+    Tier01: 1000000, // AAA+
+    Tier02: 990000, // AAA
+    Tier03: 950000, // AA+
+    Tier04: 900000, // AA
+    Tier05: 890000, // AA-
+    Tier06: 850000, // A+
+    Tier07: 800000, // A
+    Tier08: 790000, // A-
+    Tier09: 750000, // B+
+    Tier10: 700000, // B
+    Tier11: 690000, // B-
+    Tier12: 650000, // C+
+    Tier13: 600000, // C
+    Tier14: 590000, // C-
+    Tier15: 550000, // D+
+    Tier16: 500000, // D
+    Tier17: 0, // D
+  };
+  let gt = Object.entries(grade_table);
+  let final_tier = "Tier17";
+  for (let i = gt.length - 1; i >= 0; i--) {
+    const [Tier, TierScore] = gt[i];
+    if (score >= TierScore) {
+      final_tier = Tier;
     }
-    let grade_table = {
-        Tier01: 1000000, // AAA+
-        Tier02: 990000, // AAA
-        Tier03: 950000, // AA+
-        Tier04: 900000, // AA
-        Tier05: 890000, // AA-
-        Tier06: 850000, // A+
-        Tier07: 800000, // A
-        Tier08: 790000, // A-
-        Tier09: 750000, // B+
-        Tier10: 700000, // B
-        Tier11: 690000, // B-
-        Tier12: 650000, // C+
-        Tier13: 600000, // C
-        Tier14: 590000, // C-
-        Tier15: 550000, // D+
-        Tier16: 500000, // D
-        Tier17: 0, // D
-    };
-    let gt = Object.entries(grade_table);
-    let final_tier = "Tier17";
-    for (let i = gt.length - 1; i >= 0; i--) {
-        const [Tier, TierScore] = gt[i];
-        if (score >= TierScore) {
-            final_tier = Tier;
-        }
-    }
-    return final_tier;
+  }
+  return final_tier;
 };
 
 export { submitScore };
